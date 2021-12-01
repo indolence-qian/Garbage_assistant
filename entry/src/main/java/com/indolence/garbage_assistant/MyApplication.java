001580edcb1a5e3a306fd5aa0c15d74a9f7caead
qfg <2826835353@qq.com>
2021-12-01 19:58:42 +0800

    原始版本

---
+++ entry/src/main/java/com/indolence/garbage_assistant/MyApplication.java
@@ -0,0 +1,10 @@
+package com.indolence.garbage_assistant;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
