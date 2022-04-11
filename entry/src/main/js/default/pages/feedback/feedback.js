5d798189468777ea1618aed6ec15ce0c05423773
indolence-qian <2826835353@qq.com>
2022-04-11 13:57:15 +0800

    相机

--- entry/src/main/js/default/pages/feedback/feedback.js
+++ entry/src/main/js/default/pages/feedback/feedback.js
@@ -10,6 +10,7 @@ export default {
         this.text=e.value;
     },
     feedback() {
+        var time=d.getTime();
         // 每一个httpRequest对应一个http请求任务，不可复用
         let httpRequest = http.createHttp();
         // 用于订阅http响应头，此接口会比request请求先返回。可以根据业务需要订阅此消息
@@ -22,14 +23,14 @@ export default {
         });
         httpRequest.request(
         // 填写http请求的url地址，可以带参数也可以不带参数。URL地址需要开发者自定义。GET请求的参数可以在extraData中指定
-            " https://api2.bmob.cn/2/files/"+d.getDay()+"."+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+".txt",
+            "https://api2.bmob.cn/2/files/"+time+".txt",
             {
                 method: 'POST', // 可选，默认为“GET”
                 // 开发者根据自身业务需要添加header字段
                 header: {
                     'X-Bmob-Application-Id': '1f2dd70b89cd3c240cb1afaf361fe41b',
                     'X-Bmob-REST-API-Key': '9702909da94801b56e9d488842516bb3',
-                    'Content-Type': 'content-Type',
+                    'Content-Type': 'text/plain',
                 },
                 // 当使用POST请求时此字段用于传递内容
                 extraData: this.text,
--- entry/src/main/js/default/pages/home/home.js
+++ entry/src/main/js/default/pages/home/home.js
@@ -4,11 +4,15 @@ export default {
     data: {
         title: 'World'
     },
-
     to_mine() {
         router.clear();
         router.push ({
             uri: 'pages/mine/mine',
         });
+    },
+    take_photo() {
+        router.push ({
+            uri: 'pages/get_camera/get_camera',
+        })
     }
 }
